3c5b455a582a14644b0b9b03d5e5acebe680293e
rychanya <rychanya@gmail.ru>
2021-05-19 12:02:53 +0300

    mongo

--- arya_backend/auth/__init__.py
+++ arya_backend/auth/__init__.py
@@ -5,9 +5,9 @@ from fastapi import HTTPException, status
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-from arya_backend import db
 from arya_backend.config import (AUTH_ACCESS_TOKEN_EXPIRE_MINUTES,
                                  AUTH_ALGORITHM, AUTH_SECRET_KEY)
+from arya_backend.db import user as User
 from arya_backend.models.auth import TokenData
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -22,7 +22,7 @@ def get_password_hash(password):
 
 
 def authenticate_user(username: str, password: str):
-    user = db.user.get(username)
+    user = User.get(username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
