e20aa2619c5920df4eaf1e63b099d2163141d034
rychanya <rychanya@gmail.ru>
2021-05-19 13:32:58 +0300

    mongo

--- arya_backend/routers/auth.py
+++ arya_backend/routers/auth.py
@@ -24,5 +24,11 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 @router.post("/signin/", response_model=Token)
 async def signin(user: SignInUser):
     user = User.create(user.username, user.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
