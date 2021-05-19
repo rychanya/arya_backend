7e15e3618f4457448962ec16f13c42ee9f286e96
rychanya <rychanya@gmail.ru>
2021-05-19 11:55:50 +0300

    mongo

--- arya_backend/db/__init__.py
+++ arya_backend/db/__init__.py
@@ -2,8 +2,6 @@ from pymongo import MongoClient
 
 from arya_backend.config import MONGO_DB_NAME, MONGO_PASSWORD, MONGO_USER
 
-from . import user
-
 client = MongoClient(
     f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@cluster0.ys89g.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority"
 )
