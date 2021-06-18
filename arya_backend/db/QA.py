4a9251b762259bcbca46dfea4c5deaca873100f8
rychanya <rychanya@gmail.ru>
2021-06-18 21:43:17 +0300

    upload

--- arya_backend/db/QA.py
+++ arya_backend/db/QA.py
@@ -64,4 +64,5 @@ def get_or_create(qa: Optional[QA])-> Optional[QA]:
     filter = qa.dict(include={'question', 'type', 'correct'})
     # collection.find_one_and_update(filter=filter, update={})
     doc = collection.find_one(filter=filter)
-    return QA.parse_obj(doc)
+    if doc is not None:
+        return QA.parse_obj(doc)
